50a07973cc1ba66c82f9580e495e81fcacbbd3a9
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-12-10 16:01:29 +1300

    Update python-sqlite-connect.py

--- 19-5/python-sqlite-connect.py
+++ 19-5/python-sqlite-connect.py
@@ -5,7 +5,7 @@ connection = sqlite3.connect("hr.db")
 curror = connection.execute("SELECT * FROM countries") 
 list = []
 for row in curror: 
-	print row
+	print(row)
 	list.append(row)
 
 connection.close()
