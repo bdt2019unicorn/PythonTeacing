cefe6dbe4bf8d1b93de5e7a2ab9c77f16d24c23b
Hoang Thanh Binh <hoangthanh_binh8@yahoo.com>
2020-12-21 19:57:47 +1300

    python insert

--- 12-21/python-insert.py
+++ 12-21/python-insert.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 connection = sqlite3.connect("hr.db")
-# cursor = connection.execute("INSERT INTO countries(country_id, country_name, region_id) VALUES ('DE', 'defgh', 4) ")
-cursor = connection.execute("select * from countries_copy"); 
+connection.execute("INSERT INTO countries_copy(country_id, country_name, region_id) VALUES ('ZZ', 'defgh', 4) ")
+connection.commit()
+# cursor = connection.execute("select * from countries_copy"); 
 connection.close()
